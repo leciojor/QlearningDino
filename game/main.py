3d2a212d5182e096723f47d294d3ea8fc23107fc
= <leciojoaoorsiribeiro@gmail.com>
2025-04-17 00:02:27 -0600

    final

--- game/main.py
+++ game/main.py
@@ -295,7 +295,7 @@ def main(mode, agent):
         pygame.display.update()
 
 
-def menu(mode, death_count, model="model/models/q_net_2000_50000_128_64_dueling_EPOCH_200.pkl"):
+def menu(mode, death_count, model="model/models/q_net_2000_50000_128_64_dueling_EPOCH_550.pkl"):
     global points
     run = True
     while run:
